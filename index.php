29db443a72693a7772cedc6fca8ff7ccd9b5a697
Vadim Podolyan <vadim.podolyan@gmail.com>
2024-07-03 16:09:20 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Split Page with Background Image</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="left-side">
+                <div>W A S T E
+                M O V E M E N T
+                R E P O R T
+                </div>
+                <div>
+                5 – 10 Brandon Road, Islington,
+                London, N7 9AA
+                </div>
+            </div>
+            <div class="right-side">
+                <div class="container-logo">
+                    <img
+                    class="site-logo center"
+                    src="images/encore_logo_2024.png"
+                    alt="Grapefruit slice atop a pile of other slices" />
+                </div>
+                
+                <div class="partner">
+                    <div class="string1 text-align-center">In Partnership with</div>
+                    <img
+                    class="site-logo center"
+                    src="images/sample_client_logo.png"
+                    alt="Grapefruit slice atop a pile of other slices" />
+                </div>
+
+                
+
+            </div>
+        </div>
+    </body>
+    </html>
+    
