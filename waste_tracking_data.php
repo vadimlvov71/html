cd56d9a1de04672f4afd5cf32ccac4625a77422c
Vadim Podolyan <vadim.podolyan@gmail.com>
2024-07-04 17:10:24 +0300

    waste data

---
+++ waste_tracking_data.php
@@ -0,0 +1,61 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Split Page with Background Image</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="left-side-data">
+                <div class='container-data-text1'>
+                    <div class='data-text1'>Waste Tracking Data</div>
+                    <div class='data-text2'>
+                        The table below shows the distance of the waste disposal sites for each waste stream 
+                        collected from site. The waste disposal sites are selected based on the locality to the site 
+                        to minimise vehicle carbon emissions. Project totals are shown on the right of this page.
+                    </div>
+                    <div class='text3'>
+                        Project Number: M/54CA04/00056
+                    </div>
+                    <div class='text4'>
+                        <div>
+                            Insert Project duration: <span>start date – end date</span>
+                        </div>
+                        <div>
+                            Insert Reporting Month: <span> April 2024</span>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                <!--<div class="layer"></div>-->
+                
+            </div>
+            <div class="right-side">
+                <div class="container-logo">
+                    <img
+                    class="site-logo center"
+                    src="images/encore_logo_2024.png"
+                    alt="Grapefruit slice atop a pile of other slices" />
+                </div>
+                <div class="partner-text text-align-center">In Partnership with</div>
+                <div class="partner">
+                    
+                    <img
+                    class="site-logo center"
+                    src="images/sample_client_logo.png"
+                    alt="Grapefruit slice atop a pile of other slices" />
+                </div>
+
+                
+
+            </div>
+        </div>
+    </body>
+    </html>
+    
