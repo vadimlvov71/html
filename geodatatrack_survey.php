7f629ae21dd16a3feb0ea9ec7f50d307e02cb3cd
Vadim Podolyan <vadim.podolyan@gmail.com>
2024-09-04 16:33:31 +0300

    survey page

--- geodatatrack_survey.php
+++ geodatatrack_survey.php
@@ -86,15 +86,13 @@
                                         <div class='container-flex'>
                                             <div>
                                                 <div class='btn-group'>
-                                                <button type='button' class='btn btn-danger dropdown-toggle' data-toggle='dropdown' aria-expanded='false'>
-                                                Action
+                                                <button type='button' class='btn btn-outline-secondary dropdown-toggle' data-toggle='dropdown' aria-expanded='false'>
+                                                    Survey type
                                                 </button>
                                                 <div class='dropdown-menu'>
-                                                    <a class='dropdown-item' href='#'>Action</a>
-                                                    <a class='dropdown-item' href='#'>Another action</a>
-                                                    <a class='dropdown-item' href='#'>Something else here</a>
-                                                <div class='dropdown-divider'></div>
-                                                <a class='dropdown-item' href='#'>Separated link</a>
+                                                    <a class='dropdown-item' href='#'>type1</a>
+                                                    <a class='dropdown-item' href='#'>type2</a>
+                                                    <a class='dropdown-item' href='#'>type3</a>
                                                 </div>
                                                 </div>
                                             </div>
