698ab3d6f4a4b10fc313aa69ae1f1ed42e951c35
Vadim Podolyan <vadim.podolyan@gmail.com>
2024-09-06 17:18:06 +0300

    question

--- includes/data.php
+++ includes/data.php
@@ -34,26 +34,29 @@ function dropDown($id, $options){
         </select>
     </div>";
 }
-$headers_orgs=array("Name", "Address", "Contact", "Telephone", "Email", "Categories", "Metadata");
+//$headers_orgs=array("Name", "Address", "Contact", "Telephone", "Email", "Categories", "Metadata");
+$headers_orgs=array("Name", "Contact", "Contact Info", "Categories", "Metadata");
 $headers_propeties=array("Name", "Address", "Contact", "Telephone", "Email", "Metadata");
 $headers_areas=array("Name", "Long/Latit", "Categories", "Metadata");
 $headers_surveys=array("Name", "Long/Latit", "Categories", "Metadata");
 
 $value=array();
 $value['name']="ABC Org";
-$value['address']="Baker street";
-$value['contact']="aaaaaa";
-$value['telephone']="2 (999) 000-00-00";
-$value['email']="aaa@aaa.com";
+//$value['address']="Baker street";
+$value['contact']="unclear????";
+$value['contact_info']=array("telephone" => "2 (999) 000-00-00", "email" => "aaa@aaa.com", "address" => "Baker street");
+//$value['telephone']="2 (999) 000-00-00";
+//$value['email']="aaa@aaa.com";
 $value['category']=array("Region" => "Africa", "EcoRegion" => "Desert", "UN coding" => "ABC01");
 $value['metadata']="Metadata ABC";
 
 $value1=array();
 $value1['name']="DEF Org";
-$value1['address']="Douning street";
-$value1['contact']="bbbbb";
-$value1['telephone']="1 (777) 222-22-2";
-$value1['email']="bbbb@bbbbb.com";
+//$value1['address']="Douning street";
+$value['contact']="unclear????";
+$value['contact_info']=array("telephone" => "1 (777) 222-22-2", "email" => "bbbb@bbbbb.com", "address" => "Coronation street");
+//$value1['telephone']="1 (777) 222-22-2";
+//$value1['email']="bbbb@bbbbb.com";
 $value1['category']=array("Region" => "Africa", "EcoRegion" => "Savanna", "UN coding" => "DEF04");
 $value1['metadata']="Metadata DEF";
 $orgs=array();
